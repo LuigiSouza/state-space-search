21c60a6e56fede8cd0a931b51599bef4e7ea68b0
LuigiSouza <lpsouza@inf.ufsm.br>
2022-07-18 01:37:42 -0300

    feat: function to reduce edges

--- main.py
+++ main.py
@@ -1,64 +1,71 @@
+import queue
 import matplotlib.pyplot as plt
 import time
-import sys
 
-SQRT_2 = 1.41
+SQRT_2 = 14
 
 
-class Vertice:
+class Edge:
+    def __init__(self, origin: "Vertex", destiny: "Vertex", weight: int):
+        self.origin = origin
+        self.destiny = destiny
+        self.weight = weight
+
+
+class Vertex:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
-        self.arestas: dict[str, Aresta] = {}
+        self.edges: dict[str, Edge] = {}
 
     def generate_nodes(self, vertices: dict, grid: list[list]):
-        max_top = self.clearence((self.x, self.y), 0, -1, grid)
-        max_bot = self.clearence((self.x, self.y), 0, +1, grid)
-        max_left = self.clearence((self.x, self.y), -1, 0, grid)
-        max_right = self.clearence((self.x, self.y), +1, 0, grid)
+        max_top = self.__clearence((self.x, self.y), 0, -1, grid)
+        max_bot = self.__clearence((self.x, self.y), 0, +1, grid)
+        max_left = self.__clearence((self.x, self.y), -1, 0, grid)
+        max_right = self.__clearence((self.x, self.y), +1, 0, grid)
         # Explorar diagonais
         # top right
         curr = (self.x, self.y)
         local_hor_limit = max_right
         local_ver_limit = max_top
-        while self.has_diagonal(curr, 1, -1, grid):
+        while self.__has_diagonal(curr, 1, -1, grid):
             curr = (curr[0] + 1, curr[1] - 1)
-            local_hor_limit = self.clearence(curr, 1, 0, grid, local_hor_limit)
-            local_ver_limit = self.clearence(curr, 0, -1, grid, local_ver_limit)
-            if type(grid[curr[1]][curr[0]]) == Vertice:
-                self.add_aresta(grid[curr[1]][curr[0]])
+            local_hor_limit = self.__clearence(curr, 1, 0, grid, local_hor_limit)
+            local_ver_limit = self.__clearence(curr, 0, -1, grid, local_ver_limit)
+            if type(grid[curr[1]][curr[0]]) == Vertex:
+                self.add_edge(grid[curr[1]][curr[0]])
         # bottom right
         curr = (self.x, self.y)
         local_hor_limit = max_right
         local_ver_limit = max_bot
-        while self.has_diagonal(curr, 1, 1, grid):
+        while self.__has_diagonal(curr, 1, 1, grid):
             curr = (curr[0] + 1, curr[1] + 1)
-            local_hor_limit = self.clearence(curr, 1, 0, grid, local_hor_limit)
-            local_ver_limit = self.clearence(curr, 0, 1, grid, local_ver_limit)
-            if type(grid[curr[1]][curr[0]]) == Vertice:
-                self.add_aresta(grid[curr[1]][curr[0]])
+            local_hor_limit = self.__clearence(curr, 1, 0, grid, local_hor_limit)
+            local_ver_limit = self.__clearence(curr, 0, 1, grid, local_ver_limit)
+            if type(grid[curr[1]][curr[0]]) == Vertex:
+                self.add_edge(grid[curr[1]][curr[0]])
         # bottom left
         curr = (self.x, self.y)
         local_hor_limit = max_left
         local_ver_limit = max_bot
-        while self.has_diagonal(curr, -1, 1, grid):
+        while self.__has_diagonal(curr, -1, 1, grid):
             curr = (curr[0] - 1, curr[1] + 1)
-            local_hor_limit = self.clearence(curr, -1, 0, grid, local_hor_limit)
-            local_ver_limit = self.clearence(curr, 0, 1, grid, local_ver_limit)
-            if type(grid[curr[1]][curr[0]]) == Vertice:
-                self.add_aresta(grid[curr[1]][curr[0]])
+            local_hor_limit = self.__clearence(curr, -1, 0, grid, local_hor_limit)
+            local_ver_limit = self.__clearence(curr, 0, 1, grid, local_ver_limit)
+            if type(grid[curr[1]][curr[0]]) == Vertex:
+                self.add_edge(grid[curr[1]][curr[0]])
         # top left
         curr = (self.x, self.y)
         local_hor_limit = max_left
         local_ver_limit = max_top
-        while self.has_diagonal(curr, -1, -1, grid):
+        while self.__has_diagonal(curr, -1, -1, grid):
             curr = (curr[0] - 1, curr[1] - 1)
-            local_hor_limit = self.clearence(curr, -1, 0, grid, local_hor_limit)
-            local_ver_limit = self.clearence(curr, 0, -1, grid, local_ver_limit)
-            if type(grid[curr[1]][curr[0]]) == Vertice:
-                self.add_aresta(grid[curr[1]][curr[0]])
+            local_hor_limit = self.__clearence(curr, -1, 0, grid, local_hor_limit)
+            local_ver_limit = self.__clearence(curr, 0, -1, grid, local_ver_limit)
+            if type(grid[curr[1]][curr[0]]) == Vertex:
+                self.add_edge(grid[curr[1]][curr[0]])
 
-    def has_diagonal(
+    def __has_diagonal(
         self,
         origin: tuple[int, int],
         dir_x: int,
@@ -84,19 +91,54 @@ class Vertice:
             return False
         return True
 
-    def distance(self, vertice: "Vertice"):
-        dist_x = abs(self.x - vertice.x)
-        dist_y = abs(self.y - vertice.y)
-        return abs(dist_x - dist_y) + min(dist_x, dist_y) * SQRT_2
+    def distance(self, vertex: "Vertex"):
+        dist_x = abs(self.x - vertex.x)
+        dist_y = abs(self.y - vertex.y)
+        return 10 * (abs(dist_x - dist_y) + min(dist_x, dist_y) * SQRT_2)
 
-    def add_aresta(self, vertice: "Vertice"):
-        key = str(vertice.x) + "," + str(vertice.y)
-        if not self.arestas.get(key):
-            weight = self.distance(vertice)
-            self.arestas[key] = Aresta(self, vertice, weight)
-            vertice.add_aresta(self)
+    def add_edge(self, vertex: "Vertex"):
+        key = str(vertex.x) + "," + str(vertex.y)
+        if not self.edges.get(key):
+            weight = self.distance(vertex)
+            self.edges[key] = Edge(self, vertex, weight)
+            vertex.add_edge(self)
 
-    def clearence(
+    def del_edge(self, x: int, y: int):
+        key = str(x) + "," + str(y)
+        if self.edges.get(key):
+            edge = self.edges.pop(key)
+            edge.destiny.del_edge(self.x, self.y)
+
+    def __can_reduce(self, destiny: "Vertex", distance: int):
+        print("Reducing")
+        queue: list[tuple[int, Vertex]] = []
+        key = str(destiny.x) + "," + str(destiny.y)
+        for edge in self.edges:
+            if edge == key:
+                continue
+            next = self.edges[edge].destiny
+            if self.distance(next) == distance - next.distance(destiny):
+                queue.append((self.distance(next), next))
+        for new_distance, vertice in queue:
+            print("lista", queue)
+            new_distance = distance - new_distance
+            if new_distance == 0:
+                return True
+            for edge in vertice.edges:
+                next = vertice.edges[edge].destiny
+                if vertice.distance(next) == new_distance - next.distance(destiny):
+                    queue.append((vertice.distance(next), next))
+        return False
+
+    def reduce_edges(self):
+        keys_to_reduce = list(self.edges.keys())
+        for key in keys_to_reduce:
+            vertex = self.edges[key].destiny
+            edge_weight = self.edges[key].weight
+            if self.__can_reduce(vertex, edge_weight):
+                self.del_edge(vertex.x, vertex.y)
+
+    def __clearence(
         self,
         origin: tuple[int, int],
         dir_x: int,
@@ -113,23 +155,16 @@ class Vertice:
         ):
             if grid[curr[1]][curr[0]] == 0:
                 return max
-            if type(grid[curr[1]][curr[0]]) == Vertice:
-                self.add_aresta(grid[curr[1]][curr[0]])
+            if type(grid[curr[1]][curr[0]]) == Vertex:
+                self.add_edge(grid[curr[1]][curr[0]])
                 return max
             curr = (curr[0] + dir_x, curr[1] + dir_y)
             max += 1
         return max
 
 
-class Aresta:
-    def __init__(self, origin: Vertice, destiny: Vertice, weight: int):
-        self.origin = origin
-        self.destiny = destiny
-        self.weight = weight
-
-
 def create_verices(grid):
-    vertices: dict[str, Vertice] = {}
+    vertices: dict[str, Vertex] = {}
     for y, row in enumerate(grid):
         for x, value in enumerate(row):
             if value == 0:
@@ -141,7 +176,7 @@ def create_verices(grid):
                 and grid[y - 1][x - 1] == 0
                 and grid[y][x - 1] == 1 == grid[y - 1][x]
             ):
-                vertices[key] = Vertice(x, y)
+                vertices[key] = Vertex(x, y)
             # Superior Direita
             elif (
                 x > 0
@@ -149,7 +184,7 @@ def create_verices(grid):
                 and grid[y + 1][x - 1] == 0
                 and grid[y][x - 1] == 1 == grid[y + 1][x]
             ):
-                vertices[key] = Vertice(x, y)
+                vertices[key] = Vertex(x, y)
             # Inferior Direita
             elif (
                 x < len(row) - 1
@@ -157,7 +192,7 @@ def create_verices(grid):
                 and grid[y + 1][x + 1] == 0
                 and grid[y][x + 1] == 1 == grid[y + 1][x]
             ):
-                vertices[key] = Vertice(x, y)
+                vertices[key] = Vertex(x, y)
             # Inferior Esquerda
             elif (
                 x < len(row) - 1
@@ -165,7 +200,7 @@ def create_verices(grid):
                 and grid[y - 1][x + 1] == 0
                 and grid[y][x + 1] == 1 == grid[y - 1][x]
             ):
-                vertices[key] = Vertice(x, y)
+                vertices[key] = Vertex(x, y)
             else:
                 continue
             grid[y][x] = vertices[key]
@@ -187,17 +222,18 @@ def create_grid(file_name):
 
 def main():
     start = time.time()
-    grid = create_grid("input.map")
-    # grid = [
-    #     [1, 1, 1, 1, 1, 1, 0, 1],
-    #     [1, 0, 0, 1, 1, 1, 0, 1],
-    #     [1, 1, 1, 1, 1, 1, 0, 1],
-    #     [1, 1, 1, 1, 1, 1, 0, 1],
-    #     [1, 1, 1, 1, 1, 1, 1, 1],
-    # ]
+    # grid = create_grid("input.map")
+    grid = [
+        [1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 1, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+    ]
     print(f"Grid created in {time.time() - start} seconds")
     start = time.time()
     vertices = create_verices(grid)
+
     print(f"Vertices created in {time.time() - start} seconds")
 
     start = time.time()
@@ -208,14 +244,17 @@ def main():
     for x in vertices:
         vert = vertices[x]
         vert.generate_nodes(vertices, grid)
-    print(f"Grid plotted in {time.time() - start} seconds")
+    for x in vertices:
+        vert = vertices[x]
+        vert.reduce_edges()
+    print(f"Nodes generated in {time.time() - start} seconds")
     start = time.time()
     plt.imshow(plot_grid)
     edges = set()
     for x in vertices:
         vert = vertices[x]
-        for aresta in vert.arestas:
-            destiny = vert.arestas[aresta].destiny
+        for edge in vert.edges:
+            destiny = vert.edges[edge].destiny
             if (destiny.x, destiny.y, vert.x, vert.y) in edges:
                 continue
             edges.add((vert.x, vert.y, destiny.x, destiny.y))
